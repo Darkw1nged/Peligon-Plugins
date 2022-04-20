c0c9b70e8fad6849bf1406446911af1381637997
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2022-04-20 17:00:07 +0100

    20/04/22
    Initial Plugin Setup

---
+++ Peligon-PlayTime/src/main/java/net/peligon/PeligonPlayTime/Main.java
@@ -0,0 +1,74 @@
+package net.peligon.PeligonPlayTime;
+
+import net.peligon.PeligonPlayTime.commands.cmdReload;
+import net.peligon.PeligonPlayTime.libaries.CustomConfig;
+import net.peligon.PeligonPlayTime.libaries.UpdateChecker;
+import net.peligon.PeligonPlayTime.libaries.Utils;
+import net.peligon.PeligonPlayTime.libaries.storage.SQLite;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+
+@SuppressWarnings("ALL")
+public final class Main extends JavaPlugin {
+
+    public static Main getInstance;
+
+    public CustomConfig fileMessage;
+
+    public void onEnable() {
+        // ---- [ Initializing instance of main class ] ----
+        getInstance = this;
+
+        // ---- [ Loading Commands | Loading Events | Loading YML Files ] ----
+        loadCommands();
+        loadEvents();
+        saveDefaultConfig();
+
+        // ---- [ Loading lang file ] ----
+        fileMessage = new CustomConfig(this, "lang/" + this.getConfig().getString("Storage.Language File"), true);
+        fileMessage.saveDefaultConfig();
+
+        // ---- [ Checking if the server has the dependencies, if not disable ] ----
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            getServer().getConsoleSender().sendMessage(Utils.chatColor(this.fileMessage.getConfig().getString("no-plugin-dependency")));
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        // ---- [ Setting up SQLite ] ----
+        SQLite sqlLite = new SQLite();
+        sqlLite.loadTables();
+
+        // ---- [ Startup message ] ----
+        getServer().getConsoleSender().sendMessage(Utils.chatColor(this.fileMessage.getConfig().getString("startup")));
+
+        // ---- [ Check if server has most updated version ] ----
+        if (getConfig().getBoolean("check-for-updates", true)) {
+            versionChecker();
+        }
+    }
+
+    public void onDisable() {
+        // ---- [ shutdown message ] ----
+        getServer().getConsoleSender().sendMessage(Utils.chatColor(this.fileMessage.getConfig().getString("shutdown")));
+    }
+
+    public void loadCommands() {
+        getCommand("pelplaytime").setExecutor(new cmdReload());
+    }
+
+    public void loadEvents() {
+//        Arrays.asList(
+//        ).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
+    }
+
+    private void versionChecker() {
+        new UpdateChecker(this, 0).getVersion(version -> {
+            if (!version.equals(this.getDescription().getVersion())) {
+                getServer().getConsoleSender().sendMessage(Utils.chatColor(fileMessage.getConfig().getString("plugin-outdated")));
+                getServer().getConsoleSender().sendMessage(Utils.chatColor(fileMessage.getConfig().getString("plugin-link")));
+            }
+        });
+    }
+}
---
+++ Peligon-PlayTime/src/main/java/net/peligon/PeligonPlayTime/libaries/storage/SQLite.java
@@ -0,0 +1,52 @@
+package net.peligon.PeligonPlayTime.libaries.storage;
+
+import net.peligon.PeligonPlayTime.Main;
+import net.peligon.PeligonPlayTime.libaries.Utils;
+
+import java.io.File;
+import java.sql.*;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class SQLite {
+
+    public static Connection connection;
+    private final Main plugin = Main.getInstance;
+
+    public void getSQLConnection() throws SQLException {
+        File dbFolder = new File(plugin.getDataFolder(), "peligon.db");
+        connection = DriverManager.getConnection("jdbc:sqlite:" + dbFolder);
+
+        try {
+            if (connection != null) {
+                DatabaseMetaData meta = connection.getMetaData();
+            }
+            getServer().getConsoleSender().sendMessage(Utils.chatColor("&aConnection to database has been successful."));
+        } catch (SQLException e) {
+            throw new Error("Connection to database could not be made.\nPlease notify the developer of this.\n");
+        }
+    }
+
+    public void loadTables() {
+        try {
+            getSQLConnection();
+            String table = "CREATE TABLE IF NOT EXISTS player(uuid PRIMARY KEY, days INTEGER, hours INTEGER, minutes INTEGER, seconds INTEGER);";
+            String table2 = "CREATE TABLE IF NOT EXISTS server(totalPlayers INTEGER);";
+
+            try {
+                Statement statement = connection.createStatement();
+                {
+                    statement.execute(table);
+                    statement.execute(table2);
+                }
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new Error("Connection to database could not be made.\nPlease notify the developer of this.\n");
+        }
+
+    }
+
+}
