ad11cdb1335f7efdcb182285055dfe3cf2dc89f6
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2022-05-20 12:37:55 +0100

    Peligon Staff created

---
+++ Peligon-Staff/src/main/java/net/peligon/Staff/Main.java
@@ -0,0 +1,96 @@
+package net.peligon.Staff;
+
+import net.peligon.Staff.libaries.CustomConfig;
+import net.peligon.Staff.libaries.UpdateChecker;
+import net.peligon.Staff.libaries.Utils;
+import net.peligon.Staff.libaries.storage.SQLibrary;
+import net.peligon.Staff.libaries.storage.SQLiteLibrary;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
+
+public class Main extends JavaPlugin {
+
+    public static Main getInstance;
+
+    public SQLibrary sqlLibrary;
+    public String storageType = "SQLite";
+
+    public CustomConfig fileMessage;
+
+    public void onEnable() {
+        // ---- [ Initializing instance of main class ] ----
+        getInstance = this;
+
+        // ---- [ Loading Commands | Loading Events | Loading YML Files ] ----
+        loadCommands();
+        loadEvents();
+        saveDefaultConfig();
+
+        // ---- [ Loading lang file ] ----
+        fileMessage = new CustomConfig(this, "lang/" + this.getConfig().getString("Storage.lang"), true);
+        fileMessage.saveDefaultConfig();
+
+        // ---- [ Setting up storage ] ----
+        setupStorage();
+
+        // ---- [ Startup message ] ----
+        getServer().getConsoleSender().sendMessage(Utils.chatColor(this.fileMessage.getConfig().getString("startup")));
+
+        // ---- [ Check if server has most updated version ] ----
+        if (getConfig().getBoolean("check-for-updates", true)) {
+            versionChecker();
+        }
+    }
+
+    public void onDisable() {
+        // ---- [ shutdown message ] ----
+        getServer().getConsoleSender().sendMessage(Utils.chatColor(this.fileMessage.getConfig().getString("shutdown")));
+    }
+
+    public void loadCommands() {
+    }
+
+    public void loadEvents() {
+//        Arrays.asList(
+//        ).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
+    }
+
+    private void versionChecker() {
+        new UpdateChecker(this, 0).getVersion(version -> {
+            if (!version.equals(this.getDescription().getVersion())) {
+                getServer().getConsoleSender().sendMessage(Utils.chatColor(fileMessage.getConfig().getString("plugin-outdated")));
+                getServer().getConsoleSender().sendMessage(Utils.chatColor(fileMessage.getConfig().getString("plugin-link")));
+            }
+        });
+    }
+
+    private void setupStorage() {
+        if (getConfig().getString("Storage.database", "SQLite").equalsIgnoreCase("sqlite")) {
+            SQLiteLibrary sqlLite = new SQLiteLibrary();
+            sqlLite.loadTables();
+            this.storageType = "SQLite";
+        } else if (getConfig().getString("Storage.database", "SQLite").equalsIgnoreCase("MySQL")) {
+            sqlLibrary = new SQLibrary(getConfig().getString("Storage.database-settings.host"),
+                    getConfig().getInt("Storage.database-settings.port"),
+                    getConfig().getString("Storage.database-settings.database"),
+                    getConfig().getString("Storage.database-settings.username"),
+                    getConfig().getString("Storage.database-settings.password"));
+
+            if (sqlLibrary.getConnection() == null) {
+                System.out.println("Unable to establish a connection to MySQL.");
+                return;
+            }
+
+            try {
+                sqlLibrary.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Staff" +
+                        " (uuid VARCHAR(36) NOT NULL, PRIMARY KEY (uuid));").executeUpdate();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+            this.storageType = "MySQL";
+        }
+    }
+
+}
---
+++ Peligon-Staff/src/main/java/net/peligon/Staff/libaries/Utils.java
@@ -0,0 +1,91 @@
+package net.peligon.Staff.libaries;
+
+import net.peligon.Staff.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
+
+public class Utils {
+
+    private static final Main plugin = Main.getInstance;
+
+    // ---- [ Managing chat color within the plugin ] ----
+    public static String chatColor(String s) {
+        return s == null ? null : ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    // ---- [ Managing holograms for small amount of features ] ----
+    public static void moveUpHologram(String name, Location loc, int length) {
+        ArmorStand holo = loc.getWorld().spawn(loc, ArmorStand.class);
+
+        // ---- [ Settings flags for entity ] ----
+        holo.setCustomName(chatColor(name));
+        holo.setCustomNameVisible(true);
+        holo.setGravity(false);
+        holo.setInvisible(true);
+        holo.setInvulnerable(false);
+        holo.setSmall(true);
+        holo.setArms(false);
+        holo.setBasePlate(false);
+        holo.setMetadata("hologram", new FixedMetadataValue(plugin, UUID.randomUUID().toString()));
+
+        activeHolograms.put(holo, System.currentTimeMillis());
+        new BukkitRunnable() {
+            public void run() {
+                if (!activeHolograms.isEmpty() && activeHolograms.containsKey(holo)) {
+                    long timeLeft = ((activeHolograms.get(holo) / 1000) + length) - (System.currentTimeMillis() / 1000);
+                    if (timeLeft <= 0) {
+                        activeHolograms.remove(holo);
+                        holo.remove();
+                        cancel();
+                    } else {
+                        holo.teleport(new Location(holo.getWorld(), holo.getLocation().getX(), holo.getLocation().getY() + .01, holo.getLocation().getZ()));
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 1, 1);
+    }
+
+    // ---- [ Converting a lore to include colors ] ----
+    public static List<String> getConvertedLore(FileConfiguration config, String path) {
+        if (config == null) return null;
+        List<String> oldList = config.getStringList(path + ".lore");
+        List<String> newList = new ArrayList<>();
+        for (String a : oldList)
+            newList.add(ChatColor.translateAlternateColorCodes('&', a));
+        return newList;
+    }
+
+    // ---- [ Available space ] ----
+    public static boolean hasSpace(Player player, ItemStack targetItem) {
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item == null) continue;
+            if (item.getType() == targetItem.getType()) {
+                if (item.getAmount() != item.getMaxStackSize()) {
+                    item.setAmount(item.getAmount() + 1);
+                    return true;
+                }
+            }
+        }
+        if (player.getInventory().firstEmpty() != -1) {
+            player.getInventory().addItem(targetItem);
+            return true;
+        }
+        return false;
+    }
+
+    // ---- [ Check if String is only letters ] ----
+    public static boolean isOnlyLetters(String s) {
+        return !s.matches("[a-zA-Z]+");
+    }
+
+    // ---- [ Cached Items ] ----
+    public static Map<ArmorStand, Long> activeHolograms = new HashMap<>();
+}
