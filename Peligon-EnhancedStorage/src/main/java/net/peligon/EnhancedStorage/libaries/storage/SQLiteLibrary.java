46ceea4503b482f13b91c586b9b2f31b3e4fa306
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2022-06-04 19:16:11 +0100

    Enhanced Storage plugin Creation

---
+++ Peligon-EnhancedStorage/src/main/java/net/peligon/EnhancedStorage/libaries/storage/SQLiteLibrary.java
@@ -0,0 +1,51 @@
+package net.peligon.EnhancedStorage.libaries.storage;
+
+
+import net.peligon.EnhancedStorage.Main;
+import net.peligon.EnhancedStorage.libaries.Utils;
+
+import java.io.File;
+import java.sql.*;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class SQLiteLibrary {
+
+    public static Connection connection;
+    private final Main plugin = Main.getInstance;
+
+    public void getSQLConnection() throws SQLException {
+        File dbFolder = new File(plugin.getDataFolder(), "peligon.db");
+        connection = DriverManager.getConnection("jdbc:sqlite:" + dbFolder);
+
+        try {
+            if (connection != null) {
+                DatabaseMetaData meta = connection.getMetaData();
+            }
+            getServer().getConsoleSender().sendMessage(Utils.chatColor("&aConnection to database has been successful."));
+        } catch (SQLException e) {
+            throw new Error("Connection to database could not be made.\nPlease notify the developer of this.\n");
+        }
+    }
+
+    public void loadTables() {
+        try {
+            getSQLConnection();
+            String table = "CREATE TABLE IF NOT EXISTS EnhancedStorage(uuid PRIMARY KEY);";
+
+            try {
+                Statement statement = connection.createStatement();
+                {
+                    statement.execute(table);
+                }
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new Error("Connection to database could not be made.\nPlease notify the developer of this.\n");
+        }
+
+    }
+
+}
